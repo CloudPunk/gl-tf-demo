ccf97a0f771bf660652ee94d14bb723a66a13df6
cloudpunk <arkaprava.j@gmail.com>
2021-04-15 16:25:49 +0530

    mm

--- template/ec2.tf
+++ template/ec2.tf
@@ -1,6 +1,5 @@
 resource "aws_instance" "example" {
-  #ami           = data.aws_ami.amzn2.id
-  ami           = "ami-0bb8e197ca205fdb2"
+  ami           = data.aws_ami.amzn2.id
   instance_type = "t2.micro"
   user_data = data.template_file.bootstrap.rendered
 
